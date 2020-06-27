805d7257bc67a006f979fe875b89138276330f54
Kir-Antipov <kp_antipov@mail.ru>
2020-06-27 12:56:56 +0300

    HeaderFixer fixes header size and tabs' appearance

---
+++ src/fixers/header-fixer.js
@@ -0,0 +1,24 @@
+import { isRepo } from "../tools/path-detector";
+import Fixer from "./fixer";
+
+export default class HeaderFixer extends Fixer {
+    isApplieble(_, __, location) {
+        return isRepo(location);
+    }
+
+    apply(_document) {
+        let header = _document.querySelector("main > div.repohead");
+        header.className = "pagehead repohead hx_repohead readability-menu bg-gray-light pb-0 pt-3";
+
+        header.firstElementChild.className = "d-flex container-lg mb-4 px-3";
+        header.querySelector("nav").className = "js-repo-nav js-sidenav-container-pjax clearfix hx_reponav reponav px-3 container-lg";
+        header.querySelector("nav > ul").className = "list-style-none";
+
+        [...header.querySelectorAll("nav > ul > li")].forEach(tabWrapper => {
+            tabWrapper.className = "";
+            let tab = tabWrapper.querySelector("a");
+            let selected = tab.classList.contains("selected");
+            tab.className = "js-selected-navigation-item reponav-item" + (selected ? " selected" : "");
+        });
+    }
+}
