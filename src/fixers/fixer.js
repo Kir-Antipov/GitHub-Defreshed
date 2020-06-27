8625d176c7df3d288145d30347ec6fc843601cc0
Kir-Antipov <kp_antipov@mail.ru>
2020-06-27 12:55:08 +0300

    Added base class for fixers

---
+++ src/fixers/fixer.js
@@ -0,0 +1,9 @@
+export default class Fixer {
+    isApplieble(dest, src, location = window.location.href) {
+        return true;
+    }
+
+    apply(dest, src, location = window.location.href) {
+
+    }
+}
