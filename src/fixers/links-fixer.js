b5934bc70ba1c3100f64f740cd58d3853e35cddb
Kir-Antipov <kp_antipov@mail.ru>
2020-06-28 18:59:53 +0300

    Now we fix only repo links

--- src/fixers/links-fixer.js
+++ src/fixers/links-fixer.js
@@ -1,15 +1,38 @@
 import navigate from "../tools/navigate";
 import { isSameSiteURL, getAbsoluteURL } from "../tools/host-detector";
+import { isRepo } from "../tools/path-detector";
 import Fixer from "./fixer";
 
 export default class LinksFixer extends Fixer {
     apply() {
+        this._setupObserver();
+        this._fixAll();
+    }
+
+    _needToBeFixed(a) {
+        return !a.hasAttribute("defreshed") && a.href && isSameSiteURL(a.href) && isRepo(a.href);
+    }
+
+    _fix(a) {
+        a.setAttribute("defreshed", "");
+
+        a.addEventListener("click", async function(e) {
+            e.stopPropagation();
+            e.preventDefault();
+            navigate(getAbsoluteURL(this.href));
+        });
+    }
+
+    _fixAll() {
         [...document.querySelectorAll("a")]
-            .filter(link => isSameSiteURL(link.href))
-            .forEach(link => link.addEventListener("click", async function(e) {
-                e.stopPropagation();
-                e.preventDefault();
-                navigate(getAbsoluteURL(this.href));
-            }));
+        .filter(this._needToBeFixed)
+        .forEach(this._fix);
+    }
+
+    _setupObserver() {
+        if (!window.defreshObserver) {
+            window.defreshObserver = new MutationObserver(() => this._fixAll());
+            window.defreshObserver.observe(document.body, { childList: true, subtree: true });
+        }
     }
 }
--- src/fixers/window-popstate-fixer.js
+++ src/fixers/window-popstate-fixer.js
@@ -1,14 +1,14 @@
 import navigate from "../tools/navigate";
 import { isGitHub } from "../tools/host-detector";
+import { isRepo } from "../tools/path-detector";
 import Fixer from "./fixer";
 
 export default class WindowPopstateFixer extends Fixer {
     apply() {
-        window.onpopstate = function (e) {
-            if (isGitHub(document.location.href))
-                navigate(document.location.href, false);
-            else
-                window.location.href = document.location.href;
+        window.onpopstate = function () {
+            let link = document.location.href;
+            if (isGitHub(link) && isRepo(link))
+                navigate(link, false);
         };
     }
 }
