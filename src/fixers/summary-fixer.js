4cb333ea4d712ab3feb7a44a7d0a85ad866013cb
Kir-Antipov <kp_antipov@mail.ru>
2020-06-29 05:21:46 +0300

    At least they added counters

--- src/fixers/summary-fixer.js
+++ src/fixers/summary-fixer.js
@@ -55,9 +55,8 @@ export default class SummaryFixer extends Fixer {
             });
         
         if (data) {
-            let links = data.querySelectorAll("a");
-            let count = this._parseCountFromMoreLink(links);
-            let link = links[0].href;
+            let count = data.querySelector("span.Counter").innerText;
+            let link = data.querySelector("a").href;
             let svg = `<svg class="octicon octicon-tag" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M2.5 7.775V2.75a.25.25 0 01.25-.25h5.025a.25.25 0 01.177.073l6.25 6.25a.25.25 0 010 .354l-5.025 5.025a.25.25 0 01-.354 0l-6.25-6.25a.25.25 0 01-.073-.177zm-1.5 0V2.75C1 1.784 1.784 1 2.75 1h5.025c.464 0 .91.184 1.238.513l6.25 6.25a1.75 1.75 0 010 2.474l-5.026 5.026a1.75 1.75 0 01-2.474 0l-6.25-6.25A1.75 1.75 0 011 7.775zM6 5a1 1 0 100 2 1 1 0 000-2z"></path></svg>`;
 
             return this._createSummaryElement(svg, link, "package", count);
@@ -77,9 +76,8 @@ export default class SummaryFixer extends Fixer {
         let link;
         let svg = `<svg class="octicon octicon-tag" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M2.5 7.775V2.75a.25.25 0 01.25-.25h5.025a.25.25 0 01.177.073l6.25 6.25a.25.25 0 010 .354l-5.025 5.025a.25.25 0 01-.354 0l-6.25-6.25a.25.25 0 01-.073-.177zm-1.5 0V2.75C1 1.784 1.784 1 2.75 1h5.025c.464 0 .91.184 1.238.513l6.25 6.25a1.75 1.75 0 010 2.474l-5.026 5.026a1.75 1.75 0 01-2.474 0l-6.25-6.25A1.75 1.75 0 011 7.775zM6 5a1 1 0 100 2 1 1 0 000-2z"></path></svg>`;
         if (data) {
-            let links = data.querySelectorAll("a");
-            count = this._parseCountFromMoreLink(links);
-            link = links[0].href;
+            count = data.querySelector("span.Counter").innerText;
+            link = data.querySelector("a").href;
         } else {
             count = 0;
             link = getRepoURL(location) + "/releases";
@@ -143,17 +141,5 @@ export default class SummaryFixer extends Fixer {
                 })
             ]
         });
-    }
-
-    // Fix:
-    // Probably wrong result (0) for 1 release/package;
-    // Probably NaN for [1000; +∞);
-    _parseCountFromMoreLink(links) {
-        if (links.length) {
-            let match = links[links.length - 1].innerText.match(/\+ ([\d]+).*/);
-            if (match)
-                return +match[1] + 1;
-        }
-        return 0;
     }
 }
