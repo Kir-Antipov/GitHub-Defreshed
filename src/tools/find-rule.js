e2a8e97bdb7d10f9e239058e9599485111b1d006
Kir-Antipov <kp_antipov@mail.ru>
2020-07-02 08:19:21 +0300

    styleSheet.rules getter throws an exception
    during loading

--- src/tools/find-rule.js
+++ src/tools/find-rule.js
@@ -1,5 +1,13 @@
+function getRules(styleSheet) {
+    try {
+        return [...(styleSheet.rules || styleSheet.cssRules)];
+    } catch (_) {
+        return [];
+    }
+}
+
 export function findRule(selector) {
     return [...document.styleSheets].filter(styleSheet => 
-        styleSheet.href).flatMap(styleSheet => [...(styleSheet.rules || styleSheet.cssRules || [])]).find(rule => 
+        styleSheet.href).flatMap(getRules).find(rule => 
             rule.selectorText && rule.selectorText == selector);
 }
