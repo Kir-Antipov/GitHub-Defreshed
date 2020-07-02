f1ba5db8157fe62ef55f658a353af8a61c07dac5
Kir-Antipov <kp_antipov@mail.ru>
2020-07-02 03:16:29 +0300

    Enabled old-style header tabs for project page

--- src/fixers/header-fixer.js
+++ src/fixers/header-fixer.js
@@ -4,18 +4,20 @@ import Fixer from "./fixer";
 
 export default class HeaderFixer extends Fixer {
     isApplieble(location) {
-        return isRepo(location) && !isProject(location);
+        return isRepo(location);
     }
 
     waitUntilFixerReady() {
         return waitUntilReady("main:nth-child(1) > div.repohead");
     }
 
-    apply() {
+    apply(location) {
         let header = document.querySelector("main > div.repohead");
-        header.className = "pagehead repohead hx_repohead readability-menu bg-gray-light pb-0 pt-3";
 
-        header.firstElementChild.className = "d-flex container-lg mb-4 px-3";
+        if (!isProject(location)) {
+            header.className = "pagehead repohead hx_repohead readability-menu bg-gray-light pb-0 pt-3";
+            header.firstElementChild.className = "d-flex container-lg mb-4 px-3";
+        }
         header.querySelector("nav").className = "js-repo-nav js-sidenav-container-pjax clearfix hx_reponav reponav px-3 container-lg";
         header.querySelector("nav > ul").className = "list-style-none";
 
