f0bdf36b8a1ef08ecfca5f4eeb9fdfd1327bb554
Kir-Antipov <kp_antipov@mail.ru>
2020-07-10 13:49:56 +0300

    Fixed downloading releases (#8)

--- src/tools/path-detector.js
+++ src/tools/path-detector.js
@@ -77,6 +77,14 @@ export function isArchive(path = location.pathname) {
     return (getRepoPath(path) || "").startsWith("archive/");
 }
 
+export function isReleaseFile(path = location.pathname) {
+    return (getRepoPath(path) || "").startsWith("releases/download/");
+}
+
+export function isFile(path = location.pathname) {
+    return isRaw(path) || isArchive(path) || isReleaseFile(path);
+}
+
 export function isProject(path = location.pathname) {
     return /^projects\/\d+$/.test(getRepoPath(path));
 }
