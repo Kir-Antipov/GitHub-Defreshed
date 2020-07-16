02154055e1790b684eb8fd2bb86c97af078c9667
Kir-Antipov <kp_antipov@mail.ru>
2020-07-16 20:57:40 +0300

    Build

--- build/github-defreshed.meta.js
+++ build/github-defreshed.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GitHub Defreshed
 // @namespace    GitHub
-// @version      1.5.0
+// @version      1.5.5
 // @author       Kir_Antipov
 // @description  Make GitHub Great Again!
 // @license      MIT
