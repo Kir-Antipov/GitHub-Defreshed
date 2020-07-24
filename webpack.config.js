22b60b5cc444ee016b4322ae9a9912b217395109
Kir-Antipov <kp_antipov@mail.ru>
2020-07-24 07:47:21 +0300

    webpack.config downgrade

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,9 @@
 const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 const CreateFileWebpack = require("create-file-webpack");
-const StringReplacePlugin = require("string-replace-webpack-plugin");
 const { name, namespace, displayName, version, author, description, githubUser, githubRepo, license } = require("./package.json");
 
-function getMetadata(ignoreCSS = false) {
-    const metadata = `// ==UserScript==
+const metadata = `// ==UserScript==
 // @name         ${displayName}
 // @namespace    ${namespace}
 // @version      ${version}
@@ -14,17 +12,14 @@ function getMetadata(ignoreCSS = false) {
 // @license      ${license}
 // @homepageURL  https://github.com/${githubUser}/${githubRepo}
 // @updateURL    https://raw.githubusercontent.com/${githubUser}/${githubRepo}/master/build/${name}.meta.js
-// @downloadURL  https://raw.githubusercontent.com/${githubUser}/${githubRepo}/master/build/${name}${ignoreCSS ? "-simplified" : ""}.user.js
+// @downloadURL  https://raw.githubusercontent.com/${githubUser}/${githubRepo}/master/build/${name}.user.js
 // @supportURL   https://github.com/${githubUser}/${githubRepo}/issues/new
 // @match        https://github.com/*
 // @run-at       document-start
 // @grant        none
 // ==/UserScript==`;
 
-    return metadata;
-}
-
-const mainConfig = {
+module.exports = {
     mode: "production",
     entry: "./src/index.js",
     output: {
@@ -37,7 +32,7 @@ const mainConfig = {
                 terserOptions: {
                     output: {
                         beautify: false,
-                        preamble: getMetadata(),
+                        preamble: metadata,
                     },
                 },
             }),
@@ -47,47 +42,7 @@ const mainConfig = {
         new CreateFileWebpack({
             fileName: `${name}.meta.js`,
             path: path.resolve(__dirname, "build"),
-            content: getMetadata() 
+            content: metadata 
         })
     ]
 };
-
-const simplifiedConfig = {
-    mode: "production",
-    entry: "./src/index.js",
-    output: {
-        filename: `${name}-simplified.user.js`,
-        path: path.resolve(__dirname, "build"),
-    },
-    module: {
-        rules: [{ 
-            test: /all-fixers.js$/,
-            loader: StringReplacePlugin.replace({
-                replacements: [{
-                    pattern: /new \w+CSSFixer\(\),/gm,
-                    replacement: () => ""
-                }]})
-        }]
-    },
-    optimization: {
-        minimizer: [
-            new TerserPlugin({
-                terserOptions: {
-                    output: {
-                        beautify: false,
-                        preamble: getMetadata(true),
-                    },
-                },
-            }),
-        ],
-    },
-    plugins: [
-        new StringReplacePlugin()
-    ]
-};
-
-
-module.exports = [
-    mainConfig,
-    simplifiedConfig
-];
