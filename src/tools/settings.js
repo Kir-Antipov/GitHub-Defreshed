edb4756211c639510e8a90dda43e1135acac176d
Kir-Antipov <kp_antipov@mail.ru>
2020-07-26 20:18:52 +0300

    Well, I've just changed my mind

--- src/tools/settings.js
+++ src/tools/settings.js
@@ -26,7 +26,7 @@ class SettingsProperty {
 let settings = [
     new SettingsProperty("useCSS", "Use old css styles", "This will revert old css styles where it's possible. For example, it will unround edges of avatars and containers.", true),
     new SettingsProperty("defreshProfilePage", "Defresh profile page", "This will roll back the changes to the profile page interface.", true),
-    new SettingsProperty("keepProfilePageIcons", "Don't remove tab icons on the profile page", "The interface update added icons to the tabs on the profile page. I find them useful, but if you have a different opinion, you can turn them off.", true),
+    new SettingsProperty("keepProfilePageIcons", "Don't remove tab icons on the profile page", "If you want to keep tab icons on the profile page, enable this option.", false),
     new SettingsProperty("defreshProfilePageUserStatus", "Defresh status appearance on the profile page", "If you prefer the way the status looked before, then this options is for you.", true),
 ];
 
