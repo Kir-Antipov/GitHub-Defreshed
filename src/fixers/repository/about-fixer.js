bd47985dfd645a87894b21f1009bba853c2b11d2
Kir-Antipov <kp_antipov@mail.ru>
2020-07-27 04:27:02 +0300

    Added website to about section - fixes #14

--- src/fixers/repository/about-fixer.js
+++ src/fixers/repository/about-fixer.js
@@ -19,18 +19,36 @@ export default class AboutFixer extends Fixer {
         let repositoryContent = document.querySelector(".repository-content");
         let about = document.querySelector(".flex-shrink-0.col-12.col-md-3 .f4");
         let topics = repositoryContent.querySelector("div.list-topics-container.f6");
+        let websiteElement = document.querySelector(".flex-shrink-0.col-12.col-md-3 .octicon-link");
+        if (websiteElement)
+            websiteElement = websiteElement.parentElement.querySelector("a");
+        let website = websiteElement ? websiteElement.href : "";
 
-        if (about) {
-            repositoryContent
-                .prepend(createElement("div", {
-                    className: topics ? "" : "mb-3",
-                    children: [
-                        createElement("div", {
-                            className: "f4",
-                            innerHTML: about.innerHTML
-                        })
-                    ]
-                }));
-        }
+        repositoryContent
+            .prepend(createElement("div", {
+                className: topics ? "" : "mb-3",
+                children: [
+                    createElement("div", {
+                        className: "f4",
+                        children: [
+                            createElement("span", {
+                                className: "text-gray-dark mr-2",
+                                innerHTML: about.innerHTML
+                            }),
+                            website ? createElement("span", {
+                                children: [createElement("a", {
+                                    href: website,
+                                    title: website,
+                                    role: "link",
+                                    target: "_blank",
+                                    rel: "noopener noreferrer",
+                                    innerText: website
+                                })]
+                            }) : null
+                        ]
+                        .filter(x => x)
+                    })
+                ]
+            }));
     }
 }
