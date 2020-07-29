ef6aa778267219d7487c95768d2344c3504acd83
Kir-Antipov <kp_antipov@mail.ru>
2020-07-30 02:10:54 +0300

    Added preloader

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
+import { sleep } from "./tools/sleep";
 import { isGitHub } from "./tools/host-detector";
 import { defresh, isDefreshed, markAsDefreshed } from "./tools/defresh";
 
-if (!isDefreshed() && isGitHub()) {
-    markAsDefreshed();
-    defresh();
+async function start() {
+    if (!isDefreshed() && isGitHub()) {
+        markAsDefreshed();
+
+        let classList = document.documentElement.classList;
+        classList.add("defreshing");
+        await defresh();
+        classList.add("defreshed");
+        classList.remove("defreshing");
+        await sleep(1500);
+        classList.remove("defreshed");
+    }
 }
+
+start();
