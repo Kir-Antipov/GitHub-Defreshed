336362ea5a3cfdadc60f35389033ec21c3ae4bd2
Kir-Antipov <kp_antipov@mail.ru>
2020-08-28 17:51:34 +0300

    Covered `css-fixer.js` with comments

--- src/fixers/css/css-fixer.js
+++ src/fixers/css/css-fixer.js
@@ -4,15 +4,21 @@ import css from "./sass/index.scss";
 import settings from "../../tools/settings";
 import Fixer from "../fixer";
 
+/**
+ * Injects custom css styles into the page.
+ */
 export default class CSSFixer extends Fixer {
+    /** @inheritdoc */
     isApplieble() {
         return settings.useCSS.value;
     }
 
+    /** @inheritdoc */
     waitUntilFixerReady() {
         return waitUntilHeadReady();
     }
 
+    /** @inheritdoc */
     apply() {
         if (document.querySelector("head > style[defreshed]"))
             return;
