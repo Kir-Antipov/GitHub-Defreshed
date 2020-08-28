9518aa0d31fc5b503c401b5bfc1a18286c8a97bd
Kir-Antipov <kp_antipov@mail.ru>
2020-08-28 13:32:24 +0300

    Covered `container-fixer.js` with comments

--- src/fixers/repository/container-fixer.js
+++ src/fixers/repository/container-fixer.js
@@ -2,15 +2,21 @@ import { isRepo, isProject } from "../../tools/path-detector";
 import { waitUntilEntriesReady } from "../../tools/wait-until-ready";
 import Fixer from "../fixer";
 
+/**
+ * Changes container size.
+ */
 export default class ContainerFixer extends Fixer {
+    /** @inheritdoc */
     isApplieble(location) {
         return isRepo(location) && !isProject(location);
     }
 
+    /** @inheritdoc */
     waitUntilFixerReady() {
         return waitUntilEntriesReady("main:nth-child(1) .container-xl");
     }
 
+    /** @inheritdoc */
     apply() {
         document.querySelector("main .container-xl").className = "container-lg clearfix new-discussion-timeline px-3";
     }
