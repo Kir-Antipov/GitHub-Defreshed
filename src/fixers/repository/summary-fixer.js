ebe25e2585b28a8984a6c977c84d885d0e40b8c0
Kir-Antipov <kp_antipov@mail.ru>
2020-10-28 01:27:59 +0200

    Added latest release info in the summary bar
    Closes #23

--- src/fixers/repository/summary-fixer.js
+++ src/fixers/repository/summary-fixer.js
@@ -82,7 +82,22 @@ export default class SummaryFixer extends Fixer {
      */
     _createReleasesSummaryElement(location) {
         let svg = `<svg class="octicon octicon-tag" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M2.5 7.775V2.75a.25.25 0 01.25-.25h5.025a.25.25 0 01.177.073l6.25 6.25a.25.25 0 010 .354l-5.025 5.025a.25.25 0 01-.354 0l-6.25-6.25a.25.25 0 01-.073-.177zm-1.5 0V2.75C1 1.784 1.784 1 2.75 1h5.025c.464 0 .91.184 1.238.513l6.25 6.25a1.75 1.75 0 010 2.474l-5.026 5.026a1.75 1.75 0 01-2.474 0l-6.25-6.25A1.75 1.75 0 011 7.775zM6 5a1 1 0 100 2 1 1 0 000-2z"></path></svg>`;
-        return this._createSummaryElementFromRightBar(location, svg, "release", "s", 0, "", true, settings.treatTagsAsReleases.value);
+        let element = this._createSummaryElementFromRightBar(location, svg, "release", "s", 0, "", true, settings.treatTagsAsReleases.value);
+
+        if (settings.showLatestReleasePopup.value && element) {
+            let section = this._findRightBarSectionByName("releases");
+            let latestReleaseDate = section && section.querySelector("relative-time");
+            let latestReleaseContainer = ((latestReleaseDate || {}).parentElement || {}).previousElementSibling;
+            let latestReleaseVersion = latestReleaseContainer && latestReleaseContainer.querySelector("span");
+            if (latestReleaseVersion) {
+                let popupText = `${latestReleaseVersion.innerText} — ${latestReleaseDate.innerText}`;
+
+                element.classList.add("tooltipped", "tooltipped-se");
+                element.setAttribute("aria-label", popupText);
+            }
+        }
+
+        return element;
     }
 
     /**
@@ -130,11 +145,7 @@ export default class SummaryFixer extends Fixer {
      */
     _createSummaryElementFromRightBar(location, svg, text, pluralEnding = "s", defaultCount = 0, additionalPath = "", force = true, useSecondaryCount = false) {
         let plural = text + pluralEnding;
-        let data = [...document.querySelectorAll(".flex-shrink-0.col-12.col-md-3 div.BorderGrid-cell")]
-            .find(x => {
-                let link = x.querySelector("a");
-                return link && link.href.endsWith(plural);
-            });
+        let data = this._findRightBarSectionByName(plural);
 
         if (!data && !force)
             return null;
@@ -153,6 +164,21 @@ export default class SummaryFixer extends Fixer {
         }
 
         return this._createSummaryElement(svg, link, text, count, pluralEnding);
+    }
+
+    /**
+     * Finds section of the right bar by its name.
+     *
+     * @param {string} name Section's name.
+     *
+     * @returns {HTMLElement} Right bar's section, if any; otherwise, false.
+     */
+    _findRightBarSectionByName(name) {
+        return [...document.querySelectorAll(".flex-shrink-0.col-12.col-md-3 div.BorderGrid-cell")]
+            .find(x => {
+                let link = x.querySelector("a");
+                return link && link.href.endsWith(name);
+            });
     }
 
     /**
--- src/tools/settings.js
+++ src/tools/settings.js
@@ -68,6 +68,9 @@ export class SettingsProperty {
  *
  * @property {SettingsProperty<boolean>} openLanguagesByDefault
  * Open language bar by default.
+ *
+ * @property {SettingsProperty<boolean>} showLatestReleasePopup
+ * Show information about the latest release.
 */
 
 /**
@@ -83,7 +86,8 @@ export const settings = [
     new SettingsProperty("defreshProfilePageUserStatus", "Defresh status appearance on the profile page", "If you prefer the way the status looked before, then this option's for you.", true),
     new SettingsProperty("jumpToTop", "Jump to the page's top after loading", "If you want to preserve the scroll position after page load, you can disable this option.", true),
     new SettingsProperty("treatTagsAsReleases", "Treat tags as releases", "Some repositories only use pre-releases, which aren't displayed as regular releases by GitHub, so without using this option you'll see 0 as the number of releases.", true),
-    new SettingsProperty("openLanguagesByDefault", "Open language bar by default", "This option allows you to change the default state of the language bar.", false)
+    new SettingsProperty("openLanguagesByDefault", "Open language bar by default", "This option allows you to change the default state of the language bar.", false),
+    new SettingsProperty("showLatestReleasePopup", "Show information about the latest release", "This will show information about the latest release (if any) when hovering over the releases section.", true)
 ];
 for (let property of [...settings])
     settings[property.name] = property;
