1233c3b0fb37928a3889005f95ca6989b306ca37
Kir-Antipov <kp_antipov@mail.ru>
2020-10-29 05:34:41 +0200

    Build

--- build/github-defreshed.meta.js
+++ build/github-defreshed.meta.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         GitHub Defreshed
 // @namespace    GitHub
-// @version      2.1.5
+// @version      2.5.0
 // @author       Kir_Antipov
 // @description  Make GitHub Great Again!
 // @license      MIT
 // @homepageURL  https://github.com/Kir-Antipov/GitHub-Defreshed
 // @updateURL    https://raw.githubusercontent.com/Kir-Antipov/GitHub-Defreshed/master/build/github-defreshed.meta.js
 // @downloadURL  https://raw.githubusercontent.com/Kir-Antipov/GitHub-Defreshed/master/build/github-defreshed.user.js
-// @supportURL   https://github.com/Kir-Antipov/GitHub-Defreshed/issues/new
+// @supportURL   https://github.com/Kir-Antipov/GitHub-Defreshed/issues/new/choose
 // @match        https://github.com/*
 // @run-at       document-start
 // @grant        none
