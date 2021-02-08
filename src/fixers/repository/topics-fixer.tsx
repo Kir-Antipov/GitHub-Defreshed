ff288b89e3a243864eaea245a56f24facdea00df
Kir-Antipov <kp_antipov@mail.ru>
2021-02-08 05:17:32 +0200

    Ported topics-fixer to TypeScript

--- src/fixers/repository/topics-fixer.tsx
+++ src/fixers/repository/topics-fixer.tsx
@@ -1,23 +1,20 @@
-import { isRepoRoot, isRepoSetup } from "../../utils/path-detector";
-import { waitUntilElementsReady, checkIfElementsReady } from "../../utils/wait-until-ready";
-import Fixer from "../fixer";
+import { isRepoRoot, isRepoSetup } from "@utils/path-detector";
+import { waitUntilElementsReady, checkIfElementsReady } from "@utils/wait-until-ready";
+import Fixer from "@fixers/fixer";
 
 /**
  * Moves topics section to its usual location.
  */
 export default class TopicsFixer extends Fixer {
-    /** @inheritdoc */
-    isApplieble(location) {
-        return isRepoRoot(location) && !isRepoSetup(location);
+    isApplieble(location: string) {
+        return isRepoRoot(location) && !isRepoSetup();
     }
 
-    /** @inheritdoc */
     async waitUntilFixerReady() {
         return  (await waitUntilElementsReady("main:nth-child(1) .repository-content .BorderGrid-row")) &&
                 (await checkIfElementsReady("main:nth-child(1) .repository-content .BorderGrid-cell .topic-tag"));
     }
 
-    /** @inheritdoc */
     apply() {
         let firstTopic = document.querySelector("main .repository-content .BorderGrid-cell .topic-tag");
         document
