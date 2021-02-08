367b72d2f108bc8fadab2d3ebcd2a75b3644d24e
Kir-Antipov <kp_antipov@mail.ru>
2021-02-08 05:14:59 +0200

    Ported branches-fixer to TypeScript

---
+++ src/fixers/repository/branches-fixer.ts
@@ -0,0 +1,16 @@
+import { isRepoRoot, isRepoSetup, isRepoTree, isSingleFile } from "@utils/path-detector";
+import { waitUntilElementsReady } from "@utils/wait-until-ready";
+import Fixer from "@fixers/fixer";
+
+/**
+ * Waits for the list of repository branches to be loaded.
+ */
+export default class BranchesFixer extends Fixer {
+    isApplieble(location: string) {
+        return (isRepoRoot(location) || isRepoTree(location) || isSingleFile(location)) && !isRepoSetup();
+    }
+
+    waitUntilFixerReady() {
+        return waitUntilElementsReady("main:nth-child(1) #ref-list-branches");
+    }
+}
