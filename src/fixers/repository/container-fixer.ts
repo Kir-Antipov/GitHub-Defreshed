ae9b1be0df0bd869768cd83bde430a2435c2a468
Kir-Antipov <kp_antipov@mail.ru>
2021-02-08 05:15:39 +0200

    Ported container-fixer to TypeScript

--- src/fixers/repository/container-fixer.ts
+++ src/fixers/repository/container-fixer.ts
@@ -1,22 +1,19 @@
-import { isRepo, isProject } from "../../utils/path-detector";
-import { waitUntilEntriesReady } from "../../utils/wait-until-ready";
-import Fixer from "../fixer";
+import { isRepo, isProject } from "@utils/path-detector";
+import { waitUntilEntriesReady } from "@utils/wait-until-ready";
+import Fixer from "@fixers/fixer";
 
 /**
  * Changes container size.
  */
 export default class ContainerFixer extends Fixer {
-    /** @inheritdoc */
-    isApplieble(location) {
+    isApplieble(location: string) {
         return isRepo(location) && !isProject(location);
     }
 
-    /** @inheritdoc */
     waitUntilFixerReady() {
         return waitUntilEntriesReady("main:nth-child(1) .container-xl");
     }
 
-    /** @inheritdoc */
     apply() {
         document.querySelector("main .container-xl").className = "container-lg clearfix new-discussion-timeline px-3";
     }
