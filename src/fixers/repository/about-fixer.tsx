13d2d65496bc7d12d5dfd2e08f237a1accb32937
Kir-Antipov <kp_antipov@mail.ru>
2021-02-08 05:14:23 +0200

    Ported about-fixer to TypeScript

--- src/fixers/repository/about-fixer.tsx
+++ src/fixers/repository/about-fixer.tsx
@@ -1,31 +1,29 @@
-import { isRepoRoot, isRepoSetup } from "../../utils/path-detector";
-import { waitUntilElementsReady } from "../../utils/wait-until-ready";
-import Fixer from "../fixer";
+import { isRepoRoot, isRepoSetup } from "@utils/path-detector";
+import { waitUntilElementsReady } from "@utils/wait-until-ready";
+import Fixer from "@fixers/fixer";
 
 /**
  * Moves "About" section to its usual location.
  */
 export default class AboutFixer extends Fixer {
-    /** @inheritdoc */
-    isApplieble(location) {
-        return isRepoRoot(location) && !isRepoSetup(location);
+    isApplieble(location: string) {
+        return isRepoRoot(location) && !isRepoSetup();
     }
 
-    /** @inheritdoc */
     waitUntilFixerReady() {
         return waitUntilElementsReady("main:nth-child(1) .repository-content .BorderGrid-row");
     }
 
-    /** @inheritdoc */
     apply() {
         let repositoryContent = document.querySelector("main .repository-content");
         let cell = repositoryContent.querySelector(".BorderGrid-cell");
         let about = cell.querySelector(".f4");
         let firstTopic = cell.querySelector(".topic-tag");
         let topics = firstTopic ? firstTopic.parentElement : null;
-        let websiteElement = cell.querySelector(".octicon-link");
-        if (websiteElement)
+        let websiteElement = cell.querySelector<HTMLAnchorElement>(".octicon-link");
+        if (websiteElement) {
             websiteElement = websiteElement.parentElement.querySelector("a");
+        }
         let website = websiteElement ? websiteElement.href : "";
 
         repositoryContent.prepend(
