73db162e97d442d8059ce183dbec47d3d47e33f4
Kir-Antipov <kp_antipov@mail.ru>
2021-02-08 05:15:13 +0200

    Ported columns-fixer to TypeScript

--- src/fixers/repository/columns-fixer.ts
+++ src/fixers/repository/columns-fixer.ts
@@ -1,23 +1,20 @@
-import { isRepoRoot, isRepoSetup } from "../../utils/path-detector";
-import { waitUntilElementsReady } from "../../utils/wait-until-ready";
-import Fixer from "../fixer";
+import { isRepoRoot, isRepoSetup } from "@utils/path-detector";
+import { waitUntilElementsReady } from "@utils/wait-until-ready";
+import Fixer from "@fixers/fixer";
 
 /**
  * Moves all content from the goddamn columns to the
  * .repository-content container.
  */
 export default class ColumnsFixer extends Fixer {
-    /** @inheritdoc */
-    isApplieble(location) {
-        return isRepoRoot(location) && !isRepoSetup(location);
+    isApplieble(location: string) {
+        return isRepoRoot(location) && !isRepoSetup();
     }
 
-    /** @inheritdoc */
     waitUntilFixerReady() {
         return waitUntilElementsReady("main:nth-child(1) .repository-content");
     }
 
-    /** @inheritdoc */
     apply() {
         let repositoryContent = document.querySelector(".repository-content");
         let gutter = repositoryContent.querySelector(".gutter-condensed");
