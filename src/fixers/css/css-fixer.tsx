42da838c0fac79dd97267a1dd0d8c6eaaec22e5b
Kir-Antipov <kp_antipov@mail.ru>
2021-02-08 05:18:53 +0200

    Ported css-fixer to TypeScript

--- src/fixers/css/css-fixer.tsx
+++ src/fixers/css/css-fixer.tsx
@@ -1,40 +1,41 @@
-import { waitUntilHeadReady } from "../../utils/wait-until-ready";
-import { SettingsProperty } from "../../utils/settings";
-import config from "../../../package.json";
-import Fixer from "../fixer";
+import { waitUntilHeadReady } from "@utils/wait-until-ready";
+import { SettingsProperty } from "@utils/settings";
+import config from "@config";
+import Fixer from "@fixers/fixer";
 
 /**
  * Base class for css injectors.
  */
 export default class CSSFixer extends Fixer {
+    _option: SettingsProperty<boolean>;
+    _css: string;
+    _name: string;
+
     /**
      * Initializes a new instance of the CSSFixer class.
      *
-     * @param {SettingsProperty<boolean>} option
+     * @param option
      * Settings option that indicates whether to apply this fixer.
      *
-     * @param {string} css CSS to be injected.
+     * @param css CSS to be injected.
      *
-     * @param {string} name This fixer's name.
+     * @param name This fixer's name.
      */
-    constructor(option, css, name) {
+    constructor(option: SettingsProperty<boolean>, css: string, name: string) {
         super();
         this._css = css.toString();
         this._option = option;
         this._name = `${config.name}-${name}`;
     }
 
-    /** @inheritdoc */
     async isApplieble() {
         return await this._option.getValue() && !document.querySelector(`head > style[${this._name}]`);
     }
 
-    /** @inheritdoc */
     waitUntilFixerReady() {
         return waitUntilHeadReady();
     }
 
-    /** @inheritdoc */
     apply() {
         document.head.append(
             <style type="text/css" {...{ [this._name]: "" }}>
