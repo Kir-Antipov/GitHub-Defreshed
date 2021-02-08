05f4ea041dcd3a09a124cc3725f519be0abfda88
Kir-Antipov <kp_antipov@mail.ru>
2021-02-08 05:18:17 +0200

    Ported theme-selector-fixer to TypeScript

--- src/fixers/profile/theme-selector-fixer.ts
+++ src/fixers/profile/theme-selector-fixer.ts
@@ -1,23 +1,20 @@
-import { isProfile } from "../../utils/path-detector";
-import { waitUntilElementsReady, checkIfElementsReady } from "../../utils/wait-until-ready";
-import settings from "../../utils/settings";
-import Fixer from "../fixer";
+import { isProfile } from "@utils/path-detector";
+import { waitUntilElementsReady } from "@utils/wait-until-ready";
+import settings from "@utils/settings";
+import Fixer from "@fixers/fixer";
 
 /**
  * Removes theme selector from the profile page.
  */
 export default class ThemeSelectorFixer extends Fixer {
-    /** @inheritdoc */
-    async isApplieble(location) {
+    async isApplieble(location: string) {
         return await settings.defreshProfilePage.getValue() && await settings.removeThemeSelector.getValue() && isProfile(location);
     }
 
-    /** @inheritdoc */
     async waitUntilFixerReady() {
         return waitUntilElementsReady("main:nth-child(1) nav");
     }
 
-    /** @inheritdoc */
     apply() {
         let tabs = document.querySelector("main:nth-child(1) nav");
         if (tabs && tabs.nextElementSibling)
