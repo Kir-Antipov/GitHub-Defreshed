0f20d0d6df091dbe9dd5d1e248eec5d3aa15f408
Kir-Antipov <kp_antipov@mail.ru>
2021-02-08 05:06:48 +0200

    Ported convertToAsync to TypeScript

---
+++ src/utils/convert-to-async.ts
@@ -0,0 +1,26 @@
+/**
+ * Unifies asynchronous functions and functions that take a callback.
+ */
+export function convertToAsync<T>(func: Function, ...args: any[]) {
+    return new Promise<T>((resolve, reject) => {
+        try {
+            let result = func(...args, resolve);
+            if (result instanceof Promise) {
+                result.then(resolve).catch(reject);
+            } else if (result !== undefined) {
+                resolve(result);
+            }
+        } catch (e) {
+            reject(e);
+        }
+    });
+}
+
+/**
+ * Unifies asynchronous functions and functions that take a callback.
+ *
+ * @param thisObject An object to which the this keyword can refer inside the new function.
+ */
+export function bindAndConvertToAsync<T>(thisObject: any, func: Function, ...args: any[]) {
+    return convertToAsync<T>(func.bind(thisObject), ...args);
+}
