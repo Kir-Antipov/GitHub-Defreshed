dfd8e279b409a6f22615224d2996c18e7561a063
Kir-Antipov <kp_antipov@mail.ru>
2021-02-08 05:19:48 +0200

    Ported old-school-css-fixer to TypeScript

--- src/fixers/css/old-school-css-fixer.ts
+++ src/fixers/css/old-school-css-fixer.ts
@@ -1,12 +1,11 @@
 import css from "./sass/old-school-css.scss";
-import settings from "../../utils/settings";
+import settings from "@utils/settings";
 import CSSFixer from "./css-fixer";
 
 /**
  * Injects old school css styles into the page.
  */
 export default class OldSchoolCSSFixer extends CSSFixer {
-    /** @inheritdoc */
     constructor() {
         super(settings.useOldCSS, css, "old-school");
     }
