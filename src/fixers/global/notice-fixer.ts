b075f468ae2771798010c740fc7c8b9b5a9a4c02
Kir-Antipov <kp_antipov@mail.ru>
2021-02-08 05:20:21 +0200

    Ported notice-fixer to TypeScript

--- src/fixers/global/notice-fixer.ts
+++ src/fixers/global/notice-fixer.ts
@@ -1,10 +1,9 @@
-import Fixer from "../fixer";
+import Fixer from "@fixers/fixer";
 
 /**
  * Automatically closes notifications on page reload.
  */
 export default class NoticeFixer extends Fixer {
-    /** @inheritdoc */
     apply() {
         [...document.querySelectorAll(".js-notification-top-shelf[closing], div#js-flash-container[closing]")]
             .forEach(x => x.remove());
