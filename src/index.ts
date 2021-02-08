9dcde747b7086eca0a15f2ef90017982a0114772
Kir-Antipov <kp_antipov@mail.ru>
2021-02-08 05:21:49 +0200

    Ported index to TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,26 @@
-import { sleep } from "./utils/sleep";
-import { isGitHub } from "./utils/host-detector";
-import { defresh, isDefreshed, markAsDefreshed } from "./utils/defresh";
+import { sleep } from "@utils/sleep";
+import { isGitHub } from "@utils/host-detector";
+import { defresh, isDefreshed, markAsDefreshed } from "@utils/defresh";
 
 /**
  * Script entry point.
  *
  * Injects the script into the page.
  */
-async function start() {
+async function main() {
     if (!isDefreshed() && isGitHub()) {
         markAsDefreshed();
 
         let classList = document.documentElement.classList;
+
         classList.add("defreshing");
         await defresh();
         classList.add("defreshed");
+
         classList.remove("defreshing");
         await sleep(1500);
         classList.remove("defreshed");
     }
 }
 
-start();
+main();
