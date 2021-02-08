2613b4be8b2956586044c885a30e64208c5208a0
Kir-Antipov <kp_antipov@mail.ru>
2021-02-08 05:16:10 +0200

    Ported edit-details-fixer to TypeScript

--- src/fixers/repository/edit-details-fixer.ts
+++ src/fixers/repository/edit-details-fixer.ts
@@ -1,26 +1,24 @@
-import { isRepoRoot, isRepoSetup } from "../../utils/path-detector";
-import { waitUntilElementsReady, checkIfElementsReady } from "../../utils/wait-until-ready";
-import Fixer from "../fixer";
+import { isRepoRoot, isRepoSetup } from "@utils/path-detector";
+import { waitUntilElementsReady, checkIfElementsReady } from "@utils/wait-until-ready";
+import Fixer from "@fixers/fixer";
 
 /**
  * Moves repository's details editor to the top section.
  */
 export default class EditDetailsFixer extends Fixer {
-    /** @inheritdoc */
-    isApplieble(location) {
-        return isRepoRoot(location) && !isRepoSetup(location);
+    isApplieble(location: string) {
+        return isRepoRoot(location) && !isRepoSetup();
     }
 
-    /** @inheritdoc */
     async waitUntilFixerReady() {
         return  (await waitUntilElementsReady("main:nth-child(1) .repository-content .BorderGrid-row")) &&
                 (await checkIfElementsReady("main:nth-child(1) .repository-content .BorderGrid-row:nth-child(1) details"));
     }
 
-    /** @inheritdoc */
     apply() {
         let details = document.querySelector("main .repository-content .BorderGrid-row:nth-child(1) details");
-        if (details)
+        if (details) {
             document.querySelector("main .repository-content").prepend(details);
+        }
     }
 }
