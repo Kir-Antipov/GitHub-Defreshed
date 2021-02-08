afafbc2cedf4712c750b29f4e71a25e5a57ea441
Kir-Antipov <kp_antipov@mail.ru>
2021-02-08 05:20:33 +0200

    Ported window-popstate-fixer to TypeScript

--- src/fixers/global/window-popstate-fixer.ts
+++ src/fixers/global/window-popstate-fixer.ts
@@ -1,20 +1,21 @@
-import navigate from "../../utils/navigate";
-import { isGitHub } from "../../utils/host-detector";
-import { isRepo, isAnchor } from "../../utils/path-detector";
-import Fixer from "../fixer";
+import navigate from "@utils/navigate";
+import { isGitHub } from "@utils/host-detector";
+import { isRepo, isAnchor } from "@utils/path-detector";
+import Fixer from "@fixers/fixer";
 
 /**
  * Fixes popstate logic.
  */
 export default class WindowPopstateFixer extends Fixer {
-    /** @inheritdoc */
     apply() {
         window.onpopstate = function () {
             let link = document.location.href;
-            if (isGitHub(link) && isRepo(link) && !isAnchor(link))
+            if (isGitHub(link) && isRepo(link) && !isAnchor(link)) {
                 navigate(link, false);
-            else
+            }
+            else {
                 window.location.href = document.location.href;
+            }
         };
     }
 }
