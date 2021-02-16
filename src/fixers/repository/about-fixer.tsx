eba5f60674e4fc68993057e2606aef68bccd008b
Kir-Antipov <kp_antipov@mail.ru>
2021-02-16 22:11:39 +0200

    Moved About content to its own component

--- src/fixers/repository/about-fixer.tsx
+++ src/fixers/repository/about-fixer.tsx
@@ -1,5 +1,6 @@
 import { isRepoRoot, isRepoSetup } from "@utils/path-detector";
 import { waitUntilElementsReady } from "@utils/wait-until-ready";
+import About from "@components/repository/about";
 import Fixer from "@fixers/fixer";
 
 /**
@@ -15,29 +16,19 @@ export default class AboutFixer extends Fixer {
     }
 
     apply() {
-        let repositoryContent = document.querySelector("main .repository-content");
-        let cell = repositoryContent.querySelector(".BorderGrid-cell");
-        let about = cell.querySelector(".f4");
-        let firstTopic = cell.querySelector(".topic-tag");
-        let topics = firstTopic ? firstTopic.parentElement : null;
-        let websiteElement = cell.querySelector<HTMLAnchorElement>(".octicon-link");
-        if (websiteElement) {
-            websiteElement = websiteElement.parentElement.querySelector("a");
-        }
-        let website = websiteElement ? websiteElement.href : "";
+        const repositoryContent = document.querySelector("main .repository-content");
+        const cell = repositoryContent.querySelector(".BorderGrid-cell");
+        const about = cell.querySelector(".f4");
+        const website = cell
+            .querySelector(".octicon-link")
+            ?.parentElement
+            .querySelector("a")
+            ?.href;
 
         repositoryContent.prepend(
-            <div className={topics ? "" : "mb-3"}>
-                <div className="f4">
-                    <span className="text-gray-dark mr-2">{ [...about.childNodes] }</span>
-                    {
-                        website &&
-                        <span>
-                            <a href={website} title={website} role="link" target="_blank" rel="noopener noreferrer">{website}</a>
-                        </span>
-                    }
-                </div>
-            </div>
+            <About website={website}>
+                {about.childNodes}
+            </About>
         );
     }
 }
