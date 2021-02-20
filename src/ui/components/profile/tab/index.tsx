7075084b98ae88409b06cfa1ae1de2b68dce52af
Kir-Antipov <kp_antipov@mail.ru>
2021-02-20 09:13:42 +0200

    Moved Tab to its own component

---
+++ src/ui/components/profile/tab/index.tsx
@@ -0,0 +1,23 @@
+import { FC } from "jsx-dom";
+
+interface TabProps {
+    href: string;
+    icon: JSX.Element;
+    text: string;
+    count: number | string;
+    selected: boolean;
+}
+
+const Tab: FC<TabProps, JSX.Element> = ({ href, icon, text, count, selected }) => (
+    <a href={href} class={["UnderlineNav-item", selected && "selected"]}>
+        {icon}
+        {text}
+        { count != 0 &&
+            <span className="Counter" title={count as string}>
+                {count}
+            </span>
+        }
+    </a>
+);
+
+export default Tab;
