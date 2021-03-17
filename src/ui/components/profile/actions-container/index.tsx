9fb39149c03b7d69641f5850cc35c9ef8d00f5ca
Kir-Antipov <kp_antipov@mail.ru>
2021-03-17 19:36:41 +0200

    Fixed ActionsContainer

--- src/ui/components/profile/actions-container/index.tsx
+++ src/ui/components/profile/actions-container/index.tsx
@@ -2,7 +2,7 @@ import { FC } from "jsx-dom";
 
 const ActionsContainer: FC = ({ children }) => (
     <div className="pt-3">
-        {(children as Array<JSX.Element>).map(x => {
+        {[].concat([children]).map((x: HTMLElement) => {
             const summary = x?.querySelector?.("summary");
             if (summary) {
                 summary.className = "btn-link text-small muted-link my-1";
