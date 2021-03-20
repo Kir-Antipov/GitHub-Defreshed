535258fb213b806af8ec6efca8d25e784ff0fdac
Kir-Antipov <kp.antipov@gmail.com>
2021-03-21 00:26:16 +0200

    Build

--- build/github-defreshed.meta.js
+++ build/github-defreshed.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GitHub Defreshed
 // @namespace    GitHub
-// @version      4.0.1
+// @version      4.1.0
 // @author       Kir_Antipov
 // @description  Classical GitHub at its finest
 // @license      MIT
