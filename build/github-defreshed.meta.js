2cc92c92c5b5bc595d5b0a0d4879ac66837250e5
Kir-Antipov <kp.antipov@gmail.com>
2021-03-28 07:23:25 +0300

    Build

--- build/github-defreshed.meta.js
+++ build/github-defreshed.meta.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         GitHub Defreshed
 // @namespace    GitHub
-// @version      4.1.0
+// @version      4.2.0
 // @author       Kir_Antipov
 // @description  Classical GitHub at its finest
 // @license      MIT
 // @homepageURL  https://github.com/Kir-Antipov/GitHub-Defreshed
-// @updateURL    https://raw.githubusercontent.com/Kir-Antipov/GitHub-Defreshed/master/build/github-defreshed.meta.js
-// @downloadURL  https://raw.githubusercontent.com/Kir-Antipov/GitHub-Defreshed/master/build/github-defreshed.user.js
+// @updateURL    https://github.com/Kir-Antipov/GitHub-Defreshed/releases/latest/download/github-defreshed.meta.js
+// @downloadURL  https://github.com/Kir-Antipov/GitHub-Defreshed/releases/latest/download/github-defreshed.user.js
 // @supportURL   https://github.com/Kir-Antipov/GitHub-Defreshed/issues/new/choose
 // @match        https://github.com/*
 // @run-at       document-start
